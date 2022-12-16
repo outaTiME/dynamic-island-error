9392fff5d84f3219a7ee99be74a36005717360d5
outaTiME <afalduto@gmail.com>
2022-12-16 00:31:11 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const App = () => {
+  const backgroundStyle = {
+    backgroundColor: '#fafffe', // isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaProvider>
+      <View
+        style={[
+          backgroundStyle,
+          {
+            flex: 1,
+          },
+        ]}>
+        <StatusBar barStyle={'dark-content'} />
+        <View
+          style={{
+            flex: 1,
+            height: '100%',
+          }}>
+          <Navigation />
+        </View>
+      </View>
+    </SafeAreaProvider>
+  );
+};
+
+const List = () => {
+  const navBarHeight = useHeaderHeight();
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#00fffa' }}>
+      <View
+        style={{
+          backgroundColor: '#fffa00',
+          position: 'absolute',
+          top: navBarHeight,
+          width: 200,
+          height: 100,
+        }}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackVisible: false,
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerBlurEffect: 'dark',
+          headerTransparent: true,
+        }}>
+        <Stack.Screen name="Header" component={List} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
